b563a0885eee78523a79d3de0be1a3fad4e7a216
Zilas Jørgensen <Zilas2004@gmail.com>
2023-02-27 11:11:40 +0100

    Rename (Conflict)

--- SharedCalc/SharedCalc/Program.cs
+++ SharedCalc/SharedCalc/Program.cs
@@ -5,11 +5,11 @@ public class Program
     public static void Main()
     {
         Console.WriteLine("First Number");
-        double firstNum = double.Parse(Console.ReadLine());
+        double numOne = double.Parse(Console.ReadLine());
         Console.WriteLine("Sign");
         string sign = Console.ReadLine();
         Console.WriteLine("Second Number");
-        double secondNum = double.Parse(Console.ReadLine());
+        double numTwo = double.Parse(Console.ReadLine());
     }
 
     string CalcSignDecider(string sign, double numOne, double numTwo)
