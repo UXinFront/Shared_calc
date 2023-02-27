673b95edd35d188ea66da7e54766cbc7c98d30da
Niclas Hansen <32816311+UXinFront@users.noreply.github.com>
2023-02-27 12:16:52 +0100

    force

--- SharedCalc/SharedCalc/Program.cs
+++ SharedCalc/SharedCalc/Program.cs
@@ -10,17 +10,24 @@ public class Program
         string sign = Console.ReadLine();
         Console.WriteLine("Second Number");
         double secondNum = double.Parse(Console.ReadLine());
+        string result = CalcSignDecider(sign, firstNum, secondNum);
+        Console.WriteLine($"result: {result}");
     }
-
-    string CalcSignDecider(string sign, double numOne, double numTwo)
+    static string CalcSignDecider(string sign, double numOne, double numTwo)
     {
         double result = sign switch
         {
             "+" => Plus(numOne, numTwo),
             "-" => Minus(numOne, numTwo),
+            "*" => Multiplication(numOne, numTwo),
+            "/" => Divide(numOne, numTwo)
         };
+        return result.ToString();
     }
 
-    double Plus(double numOne, double numTwo) => numOne + numTwo;
-    double Minus(double numOne, double numTwo) => numOne - numTwo;
+    static double Plus(double numOne, double numTwo) => numOne + numTwo;
+    static double Minus(double numOne, double numTwo) => numOne - numTwo;
+    
+    static double Multiplication(double numOne, double numTwo) => numOne * numTwo;
+    static double Divide(double numOne, double numTwo) => numOne / numTwo;
 }
