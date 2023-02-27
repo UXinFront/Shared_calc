6efb53c8106c4c954d5510d2ea43a1fae012d586
Zilas Jørgensen <Zilas2004@gmail.com>
2023-02-27 10:47:24 +0100

    Input and Start Functions

--- SharedCalc/SharedCalc/Program.cs
+++ SharedCalc/SharedCalc/Program.cs
@@ -4,6 +4,24 @@ public class Program
 {
     public static void Main()
     {
-
+        Console.WriteLine("First Number");
+        double firstNum = double.Parse(Console.ReadLine());
+        Console.WriteLine("Sign");
+        string sign = Console.ReadLine();
+        Console.WriteLine("Second Number");
+        double secondNum = double.Parse(Console.ReadLine());
     }
+
+    string CalcSignDecider(string sign, double numOne, double numTwo)
+    {
+        double result = sign switch
+        {
+            "+" => Plus(numOne, numTwo),
+            "-" => Minus(numOne, numTwo),
+        };
+    }
+
+    double Plus(double numOne, double numTwo) => numOne + numTwo;
+    double Minus(double numOne, double numTwo) => numOne - numTwo;
+}
 }
